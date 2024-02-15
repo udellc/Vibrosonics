6a66565c00cd5fc721648f8ebc3caad0b126a38d
grant-tm <grant.t.morgan@gmail.com>
2024-02-15 13:56:59 -0800

    change var names

--- Source code/Vibrosonics/Analysis Modules/Analysis Module/AnalysisModule.h
+++ Source code/Vibrosonics/Analysis Modules/Analysis Module/AnalysisModule.h
@@ -4,6 +4,7 @@
 #include <math.h>
 
 // include derived analysis modules so they can be used by including this file
+/*
 #include "../Modules/BreadSlicer.h"
 #include "../Modules/MajorPeaks.h"
 #include "../Modules/PercussionDetection.h"
@@ -16,7 +17,7 @@
 #include "../Modules/TotalAmplitude.h"
 #include "../Modules/Centroid.h"
 #include "../Modules/DeltaAmplitudes.h"
-
+*/
 #include <AudioLab.h>
 //#define WINDOW_SIZE = 256;
 //#define WINDOW_SIZE_BY_2 = 128;
@@ -27,7 +28,7 @@ template <typename T> class AnalysisModule
 protected:
     // global constants from audio lab
     int windowSize = WINDOW_SIZE;
-    int windowSizeBy2 = WINDOW_SIZE_BY_2;
+    int windowSizeBy2 = windowSize >> 1;
     int freqWidth = FREQ_WIDTH;
     
     // input arrays from Vibrosonics
