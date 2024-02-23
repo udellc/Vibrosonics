647170b8b7bb69ef9afdef920b06224aba1924c6
grant-tm <grant.t.morgan@gmail.com>
2024-02-23 13:16:16 -0800

    MajorPeaks demo

--- Source code/Vibrosonics/Vibrosonics.ino
+++ Source code/Vibrosonics/Vibrosonics.ino
@@ -3,23 +3,15 @@
 
 Vibrosonics v = Vibrosonics();
 
-TotalAmplitude totalAmp = TotalAmplitude();
-MaxAmplitude maxAmp = MaxAmplitude();
-MeanAmplitude meanAmp = MeanAmplitude();
-Centroid cent = Centroid();
-//SalientFreqs salFreqs = SalientFreqs();
+// declare modules
+MajorPeaks mp = MajorPeaks(4);
 
 void setup() {
   v.init();
-  
-  //total.setAnalysisFreqRange(0, 500);
 
-  v.addModule(&totalAmp);
-  v.addModule(&maxAmp);
-  v.addModule(&meanAmp);
-  v.addModule(&cent);
-  //v.addModule(&salFreqs);
-  
+  mp.setAnalysisRangeByFreq(0, 1000);
+  v.addModule(&mp);
+
   Serial.printf("Ready\n");
 }
 
@@ -29,13 +21,7 @@ void loop() {
     v.processInput();
     v.analyze();
 
-    Serial.printf("total: %f\n", totalAmp.getOutput());
-    Serial.printf("max: %f\n", maxAmp.getOutput());
-    Serial.printf("mean: %f\n", meanAmp.getOutput());
-    Serial.printf("cent: %f\n", cent.getOutput());
-    
-    // map to outputs
-    //assignWaves(/* major peaks output */); // additive synthesizer
+    mp.printOutput();
 
     AudioLab.synthesize();
   } 
