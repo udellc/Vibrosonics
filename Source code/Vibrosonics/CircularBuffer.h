359d46a286ad87e6897d0645c606fce5ecf80b5e
grant-tm <grant.t.morgan@gmail.com>
2024-02-29 18:02:56 -0800

    create CircularBuffer object

---
+++ Source code/Vibrosonics/CircularBuffer.h
@@ -0,0 +1,41 @@
+#ifndef Circular_Buffer_h
+#define Circular_Buffer_h
+
+class CircularBuffer 
+{
+private:
+    // parameters
+    int numBuffers;
+    int bufferSize;
+
+    // data
+    int readHead;
+    int writeHead;
+    float **data;
+
+public:
+    // constructors / destructor
+    CircularBuffer();
+    CircularBuffer(int numBuffers, int bufferSize);
+    ~CircularBuffer();
+
+    // paramter setting
+    void setNumAndSize(int num, int size);
+    void setNumBuffers(int num);
+    void setBufferSize(int size);
+
+    // modulo
+    int smartModulo(int dividend, int divisor);
+
+    // data access
+    void write(float* buffer);
+    const float* getBuffer(int idx);
+    const float* getCurrent();
+    const float* getPrevious();
+    
+    // print buffer
+    void printAll();
+    
+};
+
+#endif
