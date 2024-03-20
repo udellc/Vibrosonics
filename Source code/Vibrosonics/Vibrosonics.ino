354157ea22342e65d2d803c1d3e3499922b1c630
grant-tm <grant.t.morgan@gmail.com>
2024-03-19 17:52:58 -0700

    test .ino setup

--- Source code/Vibrosonics/Vibrosonics.ino
+++ Source code/Vibrosonics/Vibrosonics.ino
@@ -1,52 +1,81 @@
 #include "Vibrosonics.h"
 #include "AnalysisModule.h"
 #include "CircularBuffer.h"
+#include "Pulse.h"
 
 Vibrosonics v = Vibrosonics();
 
 // declare modules
-MajorPeaks mpLow = MajorPeaks(2);
-MajorPeaks mpHigh = MajorPeaks(2);
-
+MajorPeaks mp = MajorPeaks(8);
+MajorPeaks high = MajorPeaks(4);
+PercussionDetection pd = PercussionDetection(500, 500, 0.6);
+SalientFreqs sf = SalientFreqs();
+BreadSlicer bs = BreadSlicer();
+Pulse pulses[8];
 
 void setup() {
   v.init();
 
-  mpLow.setAnalysisRangeByFreq(0, 250);
-  mpHigh.setAnalysisRangeByFreq(251, 500);
-  v.addModule(&mpLow);
-  v.addModule(&mpHigh);
+  high.setAnalysisRangeByFreq(400, 1200);
+  pd.setAnalysisRangeByFreq(1000, 4000);
+  v.addModule(&pd);
+  v.addModule(&high);
+  v.addModule(&mp);
+  v.addModule(&sf);
+  v.addModule(&bs);
+  int bands[] = {0, 200, 500, 2000, 4000};
+  bs.setBands(bands, 4);
 
   Serial.printf("Ready\n");
 }
-
 void loop() {
   if (AudioLab.ready()) {
 
     v.processInputCB();
     v.analyze();
 
-    float** mpLowOutput = new float*[2];
-    mpLowOutput = mpLow.getOutput();
+    int* testFreqs = sf.getOutput();
+    sf.printSalientFreqs();
+    //Serial.printf("Freq: %d\n", testFreqs[0]);
 
-    float** mpHighOutput = new float*[2];
-    mpHighOutput = mpHigh.getOutput();
+    float** mpOutput = mp.getOutput();
+    float* amps = bs.getOutput();
 
-    for (int i=0; i < 2; i++) {
-      //mpLowOutput[0][i] *= 0.5;
-      mpHighOutput[0][i] *= 0.5;
+    Serial.printf("BreadSlicer: ");
+    for (int i = 0; i < 4; i++) {
+      Serial.printf("%03g, ", amps[i]);
     }
- 
-    v.mapAmplitudes(mpLowOutput[1], 2, 500);
-    v.mapAmplitudes(mpHighOutput[1], 2, 500);
 
-    v.mapFrequenciesExponential(mpLowOutput[0], 2, .5);
-    v.mapFrequenciesExponential(mpHighOutput[0], 2, .5);
+    v.mapAmplitudes(mpOutput[1], 8, 1000);
 
-    v.assignWaves(mpLowOutput[0], mpLowOutput[1], 2);
-    v.assignWaves(mpHighOutput[0], mpHighOutput[1], 2);
+    for (int i = 0; i < 8; i++) {
+      if (mpOutput[0][i] > 120) break;
 
+      if (pulses[i].getPulseState() == READY) {
+        pulses[i].setAttack(mpOutput[0][i], mpOutput[1][i] * 0.8, 1);
+        pulses[i].setSustain(mpOutput[0][i], mpOutput[1][i] * 1.0, 2);
+        pulses[i].setRelease(mpOutput[0][i] * 0, mpOutput[1][i] * 0.2, 3);
+        pulses[i].setReleaseCurve(1.5);
+        pulses[i].start();
+      }
+    }
+    Pulse::update();
+
+    v.mapFrequenciesExponential(mpOutput[0], 8, .5);
+    
+    float** high_out = high.getOutput();
+    v.mapFrequenciesExponential(high_out[0], 4, 0.5);
+    v.mapAmplitudes(high_out[1], 4, 1000);
+    if(pd.getOutput()){
+      for(int i=0; i<4; i++){
+        high_out[1][i] *= 1.5;
+      }
+    }
+    for(int i=0; i<4; i++){
+      v.assignWave(high_out[0][i], high_out[1][i], 1);
+    }
+    
     AudioLab.synthesize();
-    AudioLab.printWaves();
+    //AudioLab.printWaves();
   } 
 }
