0e2fb483d0e1c0d89fb2053de801b77eda67bb9e
wwaltb <wwaltb@proton.me>
2025-03-03 16:43:03 -0800

    Update mirror mode and noisiness example comments

--- examples/mirrorMode/mirrorMode.ino
+++ examples/mirrorMode/mirrorMode.ino
@@ -2,66 +2,46 @@
 
 VibrosonicsAPI vapi = VibrosonicsAPI();
 
-Noisiness noisiness = Noisiness();
-MaxAmplitude maxAmp = MaxAmplitude();
-MeanAmplitude meanAmp = MeanAmplitude();
+// set a number of peaks for the major peaks module to find
+#define NUM_PEAKS 8
 
-MajorPeaks majorPeaks = MajorPeaks(16);
-Grain* grains = vapi.createGrainArray(4, 0, SINE);
+MajorPeaks majorPeaks = MajorPeaks(NUM_PEAKS);
 
 void setup() {
     Serial.begin(115200);
+
+    // call the API setup function
     vapi.init();
-    int lowerFreq = 300;
-    int upperFreq = 4000;
-    vapi.addModule(&noisiness, lowerFreq, upperFreq);
-    vapi.addModule(&maxAmp, lowerFreq, upperFreq);
-    vapi.addModule(&meanAmp, lowerFreq, upperFreq);
+
+    // add the major peaks analysis module
+    int lowerFreq = 20;
+    int upperFreq = 1800;
     vapi.addModule(&majorPeaks, lowerFreq, upperFreq);
-    // Shape the MajorPeaks grains
-    vapi.shapeGrainAttack(grains, 4, 1, 1.0, 1.0, 1.0);
-    vapi.shapeGrainSustain(grains, 4, 1, 1.0, 0.6);
-    vapi.shapeGrainRelease(grains, 4, 1, 1.0, 0.2, 1.0);
 }
 
 void loop() {
+    // skip if new audio window has not been recorded
     if (!AudioLab.ready()) {
         return;
     }
 
-    vapi.processInput(500);
+    // process the audio signal, filtering noise with CFAR algorithm
+    vapi.processInput(4, 4, 1.4);
+
+    // have analysis modules analyze the processed input
     vapi.analyze();
 
-    if (!isWindowNoise()) {
-        synthesizePeaks(&majorPeaks);
-    }
+    // create audio waves according the the output of the major peaks module
+    synthesizePeaks(&majorPeaks); 
 
-    vapi.updateGrains();
+    // synthesize the waves created
     AudioLab.synthesize();
+
     //AudioLab.printWaves();
 }
 
-int isWindowNoise() {
-    float energyRatio = maxAmp.getOutput() / meanAmp.getOutput();
-    float entropy = noisiness.getOutput();
-
-    if (entropy > 0.85) {
-        return true;
-    }
-
-    return false;
-}
-
 void synthesizePeaks(MajorPeaks* peaks) {
-    float* windowData = vapi.getCurrentWindow();
-    float freqData[vapi.WINDOW_SIZE_BY_2]; 
-    float ampData[vapi.WINDOW_SIZE_BY_2]; 
-    for (int i = 0; i < vapi.WINDOW_SIZE_BY_2; i++) {
-        freqData[i] = i * vapi.FREQ_RES;
-        ampData[i] = windowData[i];
-    }
     float** peaksData = peaks->getOutput();
-    vapi.mapAmplitudes(peaksData[MP_AMP], 4, 10000);
-    //vapi.assignWaves(peaksData[MP_FREQ], peaksData[MP_AMP], 4, 0);
-    vapi.triggerGrains(grains, 4, peaksData);
+    vapi.mapAmplitudes(peaksData[MP_AMP], NUM_PEAKS, 10000);
+    vapi.assignWaves(peaksData[MP_FREQ], peaksData[MP_AMP], NUM_PEAKS, 0);
 }
--- examples/noisiness/noisiness.ino
+++ examples/noisiness/noisiness.ino
@@ -7,15 +7,16 @@ MaxAmplitude maxAmp = MaxAmplitude();
 MeanAmplitude meanAmp = MeanAmplitude();
 
 MajorPeaks majorPeaks = MajorPeaks();
-Grain* grains = vapi.createGrainArray(4, 0, SINE);
 
 void setup() {
     Serial.begin(115200);
     vapi.init();
-    vapi.addModule(&noisiness);
-    vapi.addModule(&maxAmp);
-    vapi.addModule(&meanAmp);
-    vapi.addModule(&majorPeaks);
+
+    int lower_freq = 20, upper_freq = 1800;
+    vapi.addModule(&noisiness, lower_freq, upper_freq);
+    vapi.addModule(&maxAmp, lower_freq, upper_freq);
+    vapi.addModule(&meanAmp, lower_freq, upper_freq);
+    vapi.addModule(&majorPeaks, lower_freq, upper_freq);
 }
 
 void loop() {
@@ -25,12 +26,18 @@ void loop() {
 
     Serial.printf("New Window:\n");
 
-    vapi.processInput(8, 4, 1.2);
+    // process the audio signal with no noise filtering
+    vapi.processInput();
+
+    // have the analysis modules analyze the processed signal
     vapi.analyze();
 
+    // find the energy ratio and entropy (measure of uniformity) based on
+    // analysis module output
     float energyRatio = maxAmp.getOutput() / meanAmp.getOutput();
     float entropy = noisiness.getOutput();
 
+    // print the most significant peaks, energy ratio, and entropy of each window
     float** majorPeaksData = majorPeaks.getOutput();
     for (int i = 0; i < 4; i++) {
         float freq = majorPeaksData[MP_FREQ][i];
