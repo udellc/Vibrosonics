caa6b3b9d6b329e64a643f6da0bc6a5b746e05da
Ben Kahl <ben.kahl2002@gmail.com>
2025-05-01 10:17:58 -0700

    More updates to comments

--- examples/Template/Template.ino
+++ examples/Template/Template.ino
@@ -24,10 +24,12 @@ VibrosonicsAPI vapi = VibrosonicsAPI();
 // Array that stores the AudioLab input buffer time domain data
 // After FFT processing it becomes the current window's spectrogram data
 float windowData[WINDOW_SIZE_BY_2];
+
 // Stores 2 windows of raw unprocessed data
 // You may want to use raw spectrogram data if you are using the
 // noisiness module
 Spectrogram rawSpectrogram = Spectrogram(2);
+
 // Stores 2 windows of processed data
 // Most of the time you will be using processed data for analysis
 Spectrogram processedSpectrogram = Spectrogram(2);
@@ -35,6 +37,7 @@ Spectrogram processedSpectrogram = Spectrogram(2);
 // MODULES
 // Create the ModuleGroup to store initialized modules
 ModuleGroup modules = ModuleGroup(&processedSpectrogram);
+
 // Creates major peaks module to analyze defined amount of peaks
 MajorPeaks majorPeaks = MajorPeaks(NUM_PEAKS);
 
@@ -60,7 +63,7 @@ void setup()
  * 1. Check if AudioLab is ready
  * 2. Process input
  * 3. Analyze processed data with modules
- * 4. Generate waves with grains or through the API
+ * 4. Generate waves with through the API
  * 5. Synthesize and output waves through AudioLab
  */
 void loop()
@@ -80,7 +83,8 @@ void loop()
   rawSpectrogram.pushWindow(windowData);
 
   // Using this noise flooring function helps with getting a clear
-  // sounding output
+  // sounding output. This is more useful on the original prototype.
+  // You may not need this if you are using the latest hardware.
   vapi.noiseFloorCFAR(windowData, WINDOW_SIZE_BY_2, 4, 1, 1.6);
   // Push the processed data to the processed spectrogram
   processedSpectrogram.pushWindow(windowData);
@@ -90,7 +94,7 @@ void loop()
 
   // Fourth, generate waves
   // Get the analyzed data from MajorPeaks module
-  float** peaksData = majorPeaks->getOutput();
+  float** peaksData = majorPeaks.getOutput();
   // Map the amplitudes for a clearer output
   vapi.mapAmplitudes(peaksData[MP_AMP], NUM_PEAKS, 10000);
   // Assign frequency and amplitude data to be outputted on both output channels.
