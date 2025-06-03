4651ecc5747475fb97336f80b7dbe5527c906259
wwaltb <wwaltb@users.noreply.github.com>
2025-06-03 18:58:18 +0000

    deploy: be4e638036a2dff0ec68f6ccebaece9ec20628ce

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,8 +1,6 @@
 var searchData=
 [
   ['mapamplitudes_0',['mapAmplitudes',['../class_vibrosonics_a_p_i.html#a7ed4ac8be583d31702766ee6ff26065b',1,'VibrosonicsAPI']]],
-  ['mapfrequenciesexponential_1',['mapFrequenciesExponential',['../class_vibrosonics_a_p_i.html#ab354c4d31e544968ec249315929327fd',1,'VibrosonicsAPI']]],
-  ['mapfrequencieslinear_2',['mapFrequenciesLinear',['../class_vibrosonics_a_p_i.html#a21d912211eeb3a5b55cd61bf0b788dde',1,'VibrosonicsAPI']]],
-  ['mapfrequencybyoctaves_3',['mapFrequencyByOctaves',['../class_vibrosonics_a_p_i.html#a239e1ec9288e7a097fe0110149b1e123',1,'VibrosonicsAPI']]],
-  ['mapfrequencymidi_4',['mapFrequencyMIDI',['../class_vibrosonics_a_p_i.html#a1aae0297224a31b7a5f00af07cd4cdd7',1,'VibrosonicsAPI']]]
+  ['mapfrequencybyoctaves_1',['mapFrequencyByOctaves',['../class_vibrosonics_a_p_i.html#afcd86f3f2b73d50fe97499daabc46f76',1,'VibrosonicsAPI']]],
+  ['mapfrequencymidi_2',['mapFrequencyMIDI',['../class_vibrosonics_a_p_i.html#a1aae0297224a31b7a5f00af07cd4cdd7',1,'VibrosonicsAPI']]]
 ];
