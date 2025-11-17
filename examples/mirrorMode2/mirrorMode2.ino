5e895c02a8a4d00bd0e37ac492c8b4427709a466
Danielle Chang <danielle.c134@gmail.com>
2025-11-17 14:18:41 -0800

    fix: set AudioPrism window size to WINDOW_SIZE_OVERLAP

--- examples/mirrorMode2/mirrorMode2.ino
+++ examples/mirrorMode2/mirrorMode2.ino
@@ -5,7 +5,7 @@
 VibrosonicsAPI vapi = VibrosonicsAPI();
 
 float windowData[WINDOW_SIZE_BY_2];
-Spectrogram spectrogram = Spectrogram(1, WINDOW_SIZE_BY_2);
+Spectrogram spectrogram = Spectrogram(1, WINDOW_SIZE_OVERLAP);
 MajorPeaks majorPeaks = MajorPeaks(NUM_PEAKS);
 
 void setup() {
@@ -14,7 +14,7 @@ void setup() {
   // call the API setup function
   vapi.init();
 
-  majorPeaks.setWindowSize(WINDOW_SIZE_BY_2);
+  majorPeaks.setWindowSize(WINDOW_SIZE_OVERLAP);
 
   majorPeaks.setSpectrogram(&spectrogram);
 }
@@ -28,7 +28,7 @@ void loop() {
   // process the raw audio signal into frequency domain data
   vapi.processAudioInput(windowData);
 
-  for (int i = 0; i < WINDOW_SIZE_BY_2; i++) {
+  for (int i = 0; i < WINDOW_SIZE_OVERLAP; i++) {
     if (windowData[i] < 300) {
       windowData[i] = 0;
     }
@@ -45,18 +45,17 @@ void loop() {
   //AudioLab.printWaves();
 }
 
-int interpolateAroundPeak(float* data, int indexOfPeak, int sampleRate, int windowSize) {
-  float _freqRes = sampleRate * 1.0 / windowSize;
+int interpolateAroundPeak(float* data, int indexOfPeak) {
   float prePeak = indexOfPeak == 0 ? 0.0 : data[indexOfPeak - 1];
   float atPeak = data[indexOfPeak];
-  float postPeak = indexOfPeak == WINDOW_SIZE_BY_2 ? 0.0 : data[indexOfPeak + 1];
+  float postPeak = indexOfPeak == WINDOW_SIZE_OVERLAP ? 0.0 : data[indexOfPeak + 1];
   // summing around the index of maximum amplitude to normalize magnitudeOfChange
   float peakSum = prePeak + atPeak + postPeak;
   // interpolating the direction and magnitude of change, and normalizing from -1.0 to 1.0
   float magnitudeOfChange = ((atPeak + postPeak) - (atPeak + prePeak)) / (peakSum > 0.0 ? peakSum : 1.0);
 
   // return interpolated frequency
-  return int(round((float(indexOfPeak) + magnitudeOfChange) * _freqRes));
+  return int(round((float(indexOfPeak) + magnitudeOfChange) * FREQ_RES));
 }
 
 // synthesizing should generally take from raw spectrum
@@ -66,7 +65,7 @@ void synthesizePeaks(MajorPeaks* peaks) {
   vapi.mapAmplitudes(peaksData[MP_AMP], NUM_PEAKS);
 
   for (int i = 0; i < NUM_PEAKS; i++) {
-    int freq = interpolateAroundPeak(spectrogram.getCurrentWindow(), round(int(peaksData[MP_FREQ][i] * FREQ_WIDTH)), SAMPLE_RATE, WINDOW_SIZE_OVERLAP);
+    int freq = interpolateAroundPeak(spectrogram.getCurrentWindow(), round(int(peaksData[MP_FREQ][i] * FREQ_WIDTH)));
     vapi.assignWave(freq, peaksData[MP_AMP][i], 0);
     vapi.assignWave(freq, peaksData[MP_AMP][i], 1);
   }
