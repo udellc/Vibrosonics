16476fa35214383140cc675b33cf6c9daffc1f82
IvanW5X <ivanwong169@gmail.com>
2025-11-18 13:40:19 -0800

    add: add header and footer for the web app

--- WebApp/src/index.jsx
+++ WebApp/src/index.jsx
@@ -1,8 +1,11 @@
 /***************************************************************
  * File: index.jsx
+ * 
  * Date: 10/30/2025
+ * 
  * Description: The main entry point for the VibroSonics web
- * 				application built with Preact and Vite.
+ * application built with Preact and Vite.
+ * 
  * Author: Ivan Wong
  ***************************************************************/
 
@@ -11,11 +14,60 @@ import Router, { Route } from "preact-router";
 import LandingPage from "./pages/landingPage";
 import "./index.css";
 
-export function App() {
+/***************************************************************
+ * @brief
+ * 
+ * @returns 
+ ***************************************************************/
+const Header = () => {
+  return (
+    <div>
+      <h1>Maybe have a top bar for spacing and other global buttons?</h1>
+    </div>
+  );
+}
+
+/****************************************************************
+ * @brief
+ * 
+ * @returns 
+ ****************************************************************/
+const Footer = () => {
+  return (
+    <div>
+      <h1>Maybe use a footer?</h1>
+    </div>
+  );  
+}
+
+/***************************************************************
+ * @brief
+ * 
+ * @returns 
+ ***************************************************************/
+const AppContent = () => {
   return (
     <Router>
       <Route path="/" component={LandingPage} />
+      {/* TODO: maybe give these routes permissions? */}
+      {/* <Route path="/network" component={NetworkPage} /> */}
+      {/* <Route path="/modules" component={ModulesPage} /> */}
     </Router>
+  );
+}
+
+/***************************************************************
+ * @brief
+ * 
+ * @returns 
+ ***************************************************************/
+export function App() {
+  return (
+    <>
+      <Header />
+      <AppContent />
+      <Footer />
+    </>
   );
 }
 
