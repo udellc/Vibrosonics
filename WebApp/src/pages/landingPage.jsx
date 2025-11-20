0a7dce880257b4b1495ec012c413bac10bb6f420
IvanW5X <ivanwong169@gmail.com>
2025-11-19 18:10:50 -0800

    refactor: clean up landing page

--- WebApp/src/pages/landingPage.jsx
+++ WebApp/src/pages/landingPage.jsx
@@ -1,19 +1,16 @@
 /***************************************************************
  * File: landingPage.jsx
+ * 
  * Date: 11/06/2025
+ * 
  * Description: The landing page for web app
+ * 
  * Author: Ivan Wong
  ***************************************************************/
 
 import "../index.css";
-import { Knob } from "../atomics/knob";
 
-const LandingPage = () => {
-  // FIXME: remove
-  const exampleCallback = (value) => {
-    console.log("Value: " + value)
-  }
-
+const LandingPage = () => {  
   return (
     <div>
       <h1 className="font-bold mt-10">
@@ -21,11 +18,6 @@ const LandingPage = () => {
       </h1>
       <div>
         <p>Maybe a page to connect to a wifi network</p>
-        
-        {/* FIXME: remove later */}
-        <div className="w-1/2">
-          <Knob value={0} min={0} max={0} step={1} onChanged={exampleCallback}/>
-        </div>
       </div>
     </div>
   );
