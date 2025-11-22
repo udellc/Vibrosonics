4ea4c403a60a6839baf3b1824303fd3dc8a1ddbe
IvanW5X <ivanwong169@gmail.com>
2025-11-22 15:48:06 -0800

    feat: minor styling to make slider vertical

--- WebApp/src/atomics/slider.jsx
+++ WebApp/src/atomics/slider.jsx
@@ -10,24 +10,46 @@
 
 import { useState } from "preact/hooks";
 
-export default function Slider({ initialValue, min, max, step, onInput }) {
+/**
+ * @brief The Slider component is a skeleton for an audio analysis setting which uses a range based slider
+ *        foe reconfiguration
+ * 
+ * @param {Object} setting - Expanded object for the slider settings
+ * @param {String} setting.title - Name of the setting to be changed
+ * @param {Number} setting.initialValue - Initial/Default value
+ * @param {Number} setting.min - Min value the slider can be at
+ * @param {Number} setting.max - Max value the slider can be at
+ * @param {Number} setting.step - Step size for each slider increment
+ * @param {CallableFunction} setting.onInput - Callback that happens for each slider value change
+ */
+export default function Slider({ title, initialValue, min, max, step, onInput }) {
   const [value, setValue] = useState(initialValue);
 
+  // Updates the value UI for each slider movement
   const handleInput = (e) => {
     const updatedVal = Number(e.target.value);
     setValue(updatedVal);
+  };
+
+  // Calls the custom callback function when the value settles to a number
+  const handleInputChanged = (e) => {
+    const updatedVal = Number(e.target.value);
     onInput?.(updatedVal);
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <input className="w-[300px]"
+    <div className="flex flex-col w-max h-max items-center m-1 p-2">
+      <h3 className="mb-2">
+        {title}
+      </h3>
+      <input className="[writing-mode:vertical-lr] [direction:rtl]"
         type="range"
         min={min}
         max={max}
         step={step}
         value={value}
         onInput={handleInput}
+        onChange={handleInputChanged}
       />
       <span>Value: {value}</span>
     </div>
