6049c22791833f0d2e1b7ba8e02307d6e01256f3
IvanW5X <ivanwong169@gmail.com>
2025-11-22 17:12:56 -0800

    feat: implemented a working, mini file system with the SD card

--- src/main/fileSys.cpp
+++ src/main/fileSys.cpp
@@ -10,6 +10,11 @@
 
 #include "fileSys.h"
 
+/**
+ * @brief Initializes the SD card file system
+ * 
+ * @return Bool indicating if the SD card successfully initialized 
+ */
 bool FileSys::init()
 {
   bool success = true;
@@ -22,5 +27,126 @@ bool FileSys::init()
   }
   Serial.println("SD File system successfully initialized.");
 
+  listFiles("/", true);
+
   return success;
 }
+
+/**
+ * @brief Writes content to the given file in the SD card in truncate mode
+ * 
+ * @param Path - File we want to write to
+ * 
+ * @param Data - Content we want to write to the file
+ * 
+ * @return Bool inidicating of the file can be opened from the SD card
+ * 
+ * NOTE: If the file does not exist in the SD card, it will be created
+ */
+bool FileSys::writeFile(const String &Path, const String &Data)
+{
+  File file = SD.open(Path, FILE_WRITE);
+
+  if (!file)
+  {
+    return false;
+  }
+  (void) file.print(Data);
+  file.close();
+
+  return true;
+}
+
+/**
+ * @brief Writes content to the given file in the SD card in appends mode
+ * 
+ * @param Path - File we want to write to
+ * 
+ * @param Data - Content we want to write to the file
+ * 
+ * @return Bool inidicating of the file can be opened from the SD card
+ * 
+ * NOTE: If the file does not exist in the SD card, it will be created
+ */
+bool FileSys::appendFile(const String &Path, const String &Data)
+{
+  File file = SD.open(Path, FILE_APPEND);
+
+  if (!file)
+  {
+    return false;
+  }
+  (void) file.print(Data);
+  file.close();
+
+  return true;
+}
+
+/**
+ * @brief Reads the contents of the given file from the SD card and returns
+ *        the contents
+ * 
+ * @param Path - File we want ot read from
+ * 
+ * @return String representing the contents of the file
+ */
+String FileSys::readFile(const String &Path)
+{
+  File file = SD.open(Path, FILE_READ);
+
+  if (!file)
+  {
+    return "";
+  }
+  const String Data = file.readString();
+  file.close();
+
+  return Data;
+}
+
+#ifdef UPLOAD_MODE
+
+/**
+ * @brief Returns a JSON style array as a string for the files within the given directory path
+ * 
+ * @param Dir - Directory path to list files from
+ * 
+ * @param Print - Bool indicating if the files should be printed to the Serial monitor
+ * 
+ * @return JSON style array as a string, representing all files in directory of the SD card 
+ * 
+ * NOTE: This function should only exist in dev environment to view the SD card files
+ */
+String FileSys::listFiles(const String &Dir, const bool Print)
+{
+  File root = SD.open(Dir);
+
+  // Nothing found in the directory, retun empty array
+  if (!root)
+  {
+    return "[]";
+  }
+  String files = "[";
+  File file = root.openNextFile();
+  
+  while (file)
+  {
+    files += "/" + String(file.name()) + ",";
+
+    if (Print)
+    {
+      const char *Type = (file.isDirectory()) ?  "Type: Directory\t" : "Type: File\t";
+      Serial.print(Type);
+      Serial.println(file.name());
+    }
+    // TODO: this uncommented should remove all files on the SD card, will do it later once I confirm this wont break anything
+    // SD.remove(file.path());
+    file = root.openNextFile();
+  }
+  // Remove last comma
+  files.remove( (files.length() - 1) );
+  files += "]";
+
+  return files;
+}
+#endif
--- src/main/fileSys.h
+++ src/main/fileSys.h
@@ -15,6 +15,7 @@
 
 #include <SPI.h>
 #include <SD.h>
+#include "config.h"
 
 // SD card pins
 #define SCK_PIN 5
@@ -24,7 +25,28 @@
 
 namespace FileSys
 {
+  //! Inits the connected SD card
   bool init();
+
+  //! Checks for existance if a file
+  inline bool exists(const String &Path) { return ( SD.exists(Path) ); }
+
+  //! Removes a file from the SD card
+  inline bool remove(const String &Path) { return ( SD.remove(Path) ); }
+
+  //! Writes a file to the SD card, truncating the previous data
+  bool writeFile(const String &Path, const String &Data);
+
+  //! Appends data to an existing file in the SD card
+  bool appendFile(const String &Path, const String &Data);
+
+  //! Reads the contents of the given file and returns it as a string 
+  String readFile(const String &Path);
+
+  #ifdef UPLOAD_MODE
+    //! Returns a JSON array as a string for the files in the given directory
+    String listFiles(const String &Dir, const bool Print);
+  #endif
 }
 
 #endif
