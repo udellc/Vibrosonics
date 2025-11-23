ca6596cb9591b756d7f068e1317241cb26d0bc7d
IvanW5X <ivanwong169@gmail.com>
2025-11-22 22:56:45 -0800

    feat: added a hostname for the web page

--- src/main/networking.cpp
+++ src/main/networking.cpp
@@ -11,20 +11,27 @@
 
 #include "networking.h"
 #include <WiFi.h>
+#include <ESPmDNS.h>
 
-const char *defaultHostname = "vibrosonics";
-const char *apSSID = "VIBROSONICS";
+const char *defaultHostname = "vibrosonics-webapp";
+const char *apSSID = "Vibrosonics-Unsecure";
 const char *apPassword = "1234567890";
 
-// TODO: add header comment
+/**
+ * @brief Initializes WiFi capabilities on the ESP32 in access point mode, with a custom host name
+ * 
+ * @return Bool indicating of the WiFi access point has been created with the
+ *         defaultHostname domain.
+ */
 bool Networking::initAccessPoint()
 {  
   WiFi.mode(WIFI_MODE_APSTA);
   Serial.println("Starting WiFi access point...");
 
-  const bool Success = WiFi.softAP(apSSID, apPassword);
+  bool success = WiFi.softAP(apSSID, apPassword);
+  success &= MDNS.begin(defaultHostname);
 
-  if (!Success)
+  if (!success)
   {
     Serial.println("Access point creation failed.");
   }
@@ -33,20 +40,22 @@ bool Networking::initAccessPoint()
     Serial.print("Access point created. Accessible at ");
     Serial.println(WiFi.softAPIP());
   }
-
-  return Success;
+  return success;
 }
 
+// TODO: implement
 void Networking::scanAvailableNetworks()
 {
 
 }
 
+// TODO: implement
 bool Networking::connectToNetwork()
 {
   return false;
 }
 
+// TODO: implement
 bool Networking::disconnectFromNetwork()
 {
   return false;
