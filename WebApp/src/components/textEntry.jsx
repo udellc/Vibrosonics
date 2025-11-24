2e1847eef2ffafe2cec8a9ff1a05519b305ee69a
IvanW5X <ivanwong169@gmail.com>
2025-11-23 21:04:01 -0800

    add: added a generic text entry component, will be used for the enter network password and maybe more

---
+++ WebApp/src/components/textEntry.jsx
@@ -0,0 +1,17 @@
+/***************************************************************
+ * File: textEntry.jsx
+ * 
+ * Date: 11/23/2025
+ * 
+ * Description: TBD
+ * 
+ * Author: Ivan Wong
+ ***************************************************************/
+
+const TextEntry = () => {
+  return (
+    <div></div>    
+  );
+};
+
+export default TextEntry;
