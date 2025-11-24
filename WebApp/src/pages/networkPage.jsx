adb8f8fac49d207a1451a7690e3928a88f2ca12b
IvanW5X <ivanwong169@gmail.com>
2025-11-23 21:05:14 -0800

    feat: started the network page. currently can display networks given a valid networks json array

--- WebApp/src/pages/networkPage.jsx
+++ WebApp/src/pages/networkPage.jsx
@@ -9,12 +9,87 @@
  * Author: Ivan Wong
  ***************************************************************/
 
+import { useEffect, useState } from "preact/hooks";
+import { route } from "preact-router";
+import { api } from "../utils/utils";
+import NetworkCard from "../components/networkCard";
+import TextEntry from "../components/textEntry";
+
+// FIXME: remove once the backend API is implemented, just here for testing
+const networks = [
+  {
+    "wifi-ssid": "Network1",
+  },
+  {
+    "wifi-ssid": "Network2",
+  },
+  {
+    "wifi-ssid": "Network3",
+  },
+  {
+    "wifi-ssid": "Network4",
+  },
+  {
+    "wifi-ssid": "Network5",
+  }
+];
+
 const NetworkPage = () => {  
+  const [selectedNetwork, setSelectedNetwork] = useState("");
+  const [showTextForm, setTextForm] = useState(false);
+  
+  // Scan for networks on mount
+  useEffect( () => {
+    // const networks = api("GET", "/network/scan-networks");
+    console.log("Loaded Network page");
+  }, []);
+
+  const handleConnectClicked = (SSID) => {
+    setTextForm(true);
+    setSelectedNetwork(SSID);
+  };
+  const handleNetworkRequest = (Password) => {
+    const payload = { 
+      "wifi-ssid": selectedNetwork,
+      "password": Password
+    };
+    const res = api("POST", "/network/network-request", payload);
+
+    // Ok, route to modules page
+    if (res["status"] == 200) {
+      route("/modules")
+    }
+    else {
+      // TODO: error handle
+      console.log("Invalid credentials");
+    }
+  }
+
   return (
-    <div>
-      <h1 className="font-bold mt-10">
-        Network page
-      </h1>
+    <div className="mt-20 ml-10 mr-10">
+      {/* Centered vertical layout */}
+      <div className="flex flex-col items-center">
+        <h1 className="font-bold mt-10 text-4xl">
+          Available Networks
+        </h1>
+        { networks.map( (network) => {
+          return (
+            <NetworkCard 
+              key={network["wifi-ssid"]}
+              SSID={network["wifi-ssid"]}
+              onConnect={() => {
+                handleConnectClicked(network["wifi-ssid"]);
+              }}/>
+          );
+        })}
+        {showTextForm === true ? (
+          // TODO: replace this with a text form component
+          <TextEntry />
+        ) : (
+          // Show nothing
+          <></>
+        ) }
+      </div>
     </div>
   );
 };
