05e30a0813e7227bd5b749a64fdf55b196d816ae
IvanW5X <ivanwong169@gmail.com>
2026-02-17 17:18:54 -0800

    update: moved previous upload fix into successful init condtion branch

--- main/fileSys.cpp
+++ main/fileSys.cpp
@@ -27,14 +27,14 @@ bool FileSys::init()
   }
   else
   {
+    // Create necessary directories if not found on SD card
+    if (!SD.exists("/assets"))
+      SD.mkdir("/assets");
+    if (!SD.exists("/data"))
+      SD.mkdir("/data");
+
     DEBUG_PRINTLN("DEBUG: SD File system successfully initialized.");
   }
-  // Create necessary directories if not found on SD card
-  if (!SD.exists("/assets"))
-    SD.mkdir("/assets");
-  if (!SD.exists("/data"))
-    SD.mkdir("/data");
-
   return success;
 }
 
