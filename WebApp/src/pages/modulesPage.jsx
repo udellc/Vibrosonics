dca9595f0103a71cc0e2cc097784f5d3a9797b00
IvanW5X <ivanwong169@gmail.com>
2026-02-19 00:34:44 -0800

    add: started getSettings() on mount callback

--- WebApp/src/pages/modulesPage.jsx
+++ WebApp/src/pages/modulesPage.jsx
@@ -9,9 +9,36 @@
  * Author: Ivan Wong
  ***************************************************************/
 
+import { useContext, useEffect, useState } from "preact/hooks";
 import AnalysisModule from "../components/analysisModule";
+import { api, HTTP_STATUS } from "../utils/utils";
+import { AudioSettingsContext } from "../utils/configurations";
 
 const ModulesPage = () => {
+  const { analysisSettings } = useContext(AudioSettingsContext);
+
+  const [globalSettings, setGlobalSettings] = useState({});
+  const [modules, setModules] = useState([]);
+
+  /**
+   * @brief
+   */
+  const getSettings = async () => {
+    // const res = await api("GET", "/audio/getSettings");
+
+    // if (res.status == HTTP_STATUS.OK) {
+      // console.log(analysisSettings);
+      // setGlobalSettings(res.data.globalSettings);
+      // setModules(res.data.modules);
+    // }
+  };
+
+  useEffect( () => {
+    // TODO: add API to get loaded settings
+    getSettings();    
+  }
+  ,[]);
+
   return (
     <div>
       <h1 className="font-bold mt-10">
