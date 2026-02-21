7c4fcc005c720dec17e6cc77b982b7438c387c8f
IvanW5X <ivanwong169@gmail.com>
2026-02-20 17:04:47 -0800

    add: added helper factory for making modules based on type and added safe values for global settings

--- main/utils.cpp
+++ main/utils.cpp
@@ -21,22 +21,22 @@ using ModuleFactory = std::map<ModuleType, CreateModule>;
 // TODO: add header comment
 void Utils::populateGlobalSettings(JsonObject& global, AnalysisConfig* config)
 {
-  config->noiseFloor = global["noiseFloor"];
-  config->cfarRefCount = global["cfarRefCount"];
-  config->cfarGuardCount = global["cfarGuardCount"];
-  config->cfarBias = global["cfarBias"];
-  config->smoothingFactor = global["smoothingFactor"];
+  config->noiseFloor = global["noiseFloor"] | 280.0f;
+  config->cfarRefCount = global["cfarRefCount"] | 6;
+  config->cfarGuardCount = global["cfarGuardCount"] | 1;
+  config->cfarBias = global["cfarBias"] | 1.2f;
+  config->smoothingFactor = global["smoothingFactor"] | 0.2f;
 }
 
 // TODO: add header comment and implement
 void Utils::populateModulesList(JsonArray& modulesList, AnalysisConfig* config)
 {
-  ModuleConfig** modules = config->modules;
+  // ModuleConfig** modules = config->modules.get();
 
   for (const auto module : modulesList)
   {
     const auto Type = module["type"];
-    DEBUG_PRINTF("DEBUG: module type: %s\n", Type);
+    // DEBUG_PRINTF("DEBUG: module type: %s\n", Type);
 
     // TODO: use createModule then add params then add to config argument
   }
@@ -49,8 +49,8 @@ inline ModulePtr Utils::createModule(const ModuleType Type)
   // TODO: add more modules types as we create structs for them
   static const ModuleFactory Map =
   {
-    { MAJORPEAKS, []() { return std::make_unique<MajorPeaksConfig>(MAJORPEAKS, 0, 0, NONE, 10000.0, 1); } }
+    { MAJORPEAKS, []() { return std::make_unique<MajorPeaksConfig>(0, 0, NONE, 10000.0, 1); } }
   };
-  auto itr = Map.find(type);
-  return (itr == Map.end() ? nullptr : itr.second());
+  auto itr = Map.find(Type);
+  return (itr == Map.end() ? nullptr : itr->second());
 }
