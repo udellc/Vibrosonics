cc3894b227b1b43cb0f9a59270038aa7f27bd642
IvanW5X <ivanwong169@gmail.com>
2026-02-23 17:01:56 -0800

    cleanup: formatting changes

--- WebApp/src/components/footer.jsx
+++ WebApp/src/components/footer.jsx
@@ -15,7 +15,7 @@
  */
 const Footer = () => {
   return (
-    <div className="font-bold mt-10 border-t-2 border-black p-4 flex w-full h-[10vh]">
+    <div className="font-bold mt-10 border-t-2 border-black p-4 flex w-full min-h-[10vh]">
       <p>
         Developed by the 2024-26 Vibrosonics Software Teams under the leadership
         of Dr. Chet Udell, in collaboration with Cymaspace and contributors Ivan
--- WebApp/src/components/header.jsx
+++ WebApp/src/components/header.jsx
@@ -17,38 +17,32 @@ import { api, HTTP_STATUS } from "../utils/utils";
  * @returns Header component
  */
 const Header = () => {
-  const [data, setData] = useState("");
   const printMemory = async () => {
     const res = await api("GET", "/dev/getMemory");
 
     if (res?.status == HTTP_STATUS.OK) {
-      setData(res.data);
+      console.log("Check serial monitor");
     }
-  }
+  };
 
   return (
-    <div className="p-4 flex w-full h-[10vh] border-b-2 border-black justify-between">
-
+    <div className="p-4 flex w-full min-h-[8vh] border-b-2 border-black justify-between">
+      
       {/* Left side */}
       <div className="flex flex-row justify-between">
-
-        {/* Logo can go here */}
         {/* TODO: use the vibrosonics logo file in an assets folder under WebApp/assets and link img here */}
-        <h2 className="font-bold text-3xl ml-2">
-          Vibrosonics
-        </h2>
+        <h2 className="font-bold text-3xl ml-2">Vibrosonics</h2>
       </div>
 
       {/* Right side */}
       <div>
-        <div>DEBUGGIN
-          <button
-            className="bg-cyan-400 cursor-pointer"
-            onClick={printMemory}
-          >PRINT MEMORY</button>
-          {data}
+        <div>
+          DEBUGGING
+          <button className="ml-3 bg-cyan-400 cursor-pointer" onClick={printMemory}>
+            PRINT MEMORY BTN
+          </button>
         </div>
-        <div>Network stuff</div>
+        <div>TODO: menu bar? Network stuff?</div>
       </div>
     </div>
   );
