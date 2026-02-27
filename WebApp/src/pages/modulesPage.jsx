a469e7a66ddf7e5536c0f653b512e80b94e29aab
IvanW5X <ivanwong169@gmail.com>
2026-02-26 23:15:48 -0800

    feat: made channel dropbox only display selected channel modules

--- WebApp/src/pages/modulesPage.jsx
+++ WebApp/src/pages/modulesPage.jsx
@@ -13,34 +13,21 @@ import { useContext, useEffect, useState } from "preact/hooks";
 import AnalysisModule from "../components/analysisModule";
 import { api, HTTP_STATUS } from "../utils/utils";
 import { AudioSettingsContext } from "../utils/configurations";
-import { createProject } from "../utils/configurations.js";
 import GlobalSettings from "../components/globalSettings";
-import Checkbox from "../atomics/checkbox";
-import EQ_PRESETS from "../data/eqSettings.json";
-
-// TODO: remove hwne done testing
-import globalsEx from "../data/dataExample_global.json";
-import modulesEx from "../data/dataExample_modules.json";
 import ConfigManager from "../components/configManager";
 
 const ModulesPage = () => {
   // Persistant memory/data
-  // const { globalSettings, setGlobalSettings } =
-  //   useContext(AudioSettingsContext);
-  // const { modules, setModules } = useContext(AudioSettingsContext);
-
-  // TODO: remove when done testing and uncomment ^^
-  const [globalSettings, setGlobalSettings] = useState(globalsEx);
-  const [modules, setModules] = useState(modulesEx);
+  const { globalSettings, setGlobalSettings } =
+    useContext(AudioSettingsContext);
+  const { modules, setModules } = useContext(AudioSettingsContext);
 
   // UI stuff
   const [selectedChannel, setSelectedChannel] = useState(0);
-  const [displayedModules, setDisplayedModules] = useState([]);
+  const [displayedModules, setDisplayedModules] = useState([])
 
-  const handleDropdownChange = (val) => {
-    // TODO: implement
-    console.log(val.target.value);
-    setSelectedChannel(val);
+  const handleDropdownChange = (e) => {
+    setSelectedChannel(e.target.value);
   };
 
   /**
@@ -50,13 +37,19 @@ const ModulesPage = () => {
     const res = await api("GET", "/analysis/getSettings");
 
     if (res.status == HTTP_STATUS.OK) {
-      setGlobalSettings(res.data?.global || {});
-      setModules(res.data?.modules || []);
-
-      console.log(res.data?.global);
-      console.log(res.data?.modules);
+      setGlobalSettings(res.data?.global ?? {});
+      setModules(res.data?.modules ?? []);
     }
   };
+  useEffect(() => {
+    // Get filtered modules based on channel, preserving the index number for updates to the context
+    const filtered = modules
+      .map((m, index) => ({ ...m, index }))
+      .filter(m => Number(m.outputNumber) === Number(selectedChannel));
+
+    setDisplayedModules(filtered);
+  }, [modules, selectedChannel]);
+
   /**
    * @brief Gets the analysis configurations on mount
    */
@@ -67,12 +60,12 @@ const ModulesPage = () => {
   return (
     <div className="flex flex-col m-8">
       <ConfigManager>
-        <div>
+        <>
           <div className="flex flex-col">
             <h4 className="font-bold text-lg">Output Channel:</h4>
             <select
               className="p-1 h-10 w-20 bg-blue-300 mb-4"
-              onChange={(val) => handleDropdownChange(val)}
+              onChange={(e) => handleDropdownChange(e)}
             >
               <option value={0}>0</option>
               <option value={1}>1</option>
@@ -83,19 +76,19 @@ const ModulesPage = () => {
               globalSettings={globalSettings}
               setGlobalSettings={setGlobalSettings}
             />
-            {modules.map((m, index) => {
+            {displayedModules?.map((m) => {
               return (
-                <div className="gap-3">
+                <div key={m.index} className="gap-3">
                   <AnalysisModule
-                    index={index}
-                    module={m}
+                    index={m.index}
+                    module={modules[m.index]}
                     setModules={setModules}
                   />
                 </div>
               );
             })}
           </div>
-        </div>
+        </>
       </ConfigManager>
     </div>
   );
