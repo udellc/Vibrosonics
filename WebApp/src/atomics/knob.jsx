c14a0aca2dd3eef0954dc813849b3a1c97dabb81
IvanW5X <ivanwong169@gmail.com>
2026-02-26 23:12:16 -0800

    feat: moved text entry input to knob component

--- WebApp/src/atomics/knob.jsx
+++ WebApp/src/atomics/knob.jsx
@@ -22,7 +22,6 @@ import { useEffect, useRef } from "preact/hooks";
  * @param {Number} setting.step - Step size for each knob increment
  * @param {CallableFunction} setting.onChange - Callback that happens for each knob value change
  */
-
 export default function Knob({
   min = 0,
   max = 10,
@@ -66,6 +65,14 @@ export default function Knob({
     window.removeEventListener("mouseup", handleMouseUp);
   };
 
+  const handleSpinboxChange = (e) => {
+    const val =
+      e.target instanceof HTMLInputElement ? Number(e.target.value) : value;
+    const clampedVal = Math.max(min, Math.min(max, val));
+
+    if (onChange) onChange(clampedVal);
+  };
+
   useEffect(() => {
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
@@ -80,7 +87,7 @@ export default function Knob({
       <div className="w-[100px] h-[100px] flex items-center justify-center relative">
         <div
           onMouseDown={handleMouseDown}
-          className="group w-[75px] h-[75px] bg-gray-800 border-2 border-gray-600 rounded-full relative cursor-ns-resize shadow-lg active:border-yellow-400 active:border-yellow-400 transition-colors"
+          className="group w-[75px] h-[75px] bg-gray-800 border-2 border-gray-600 rounded-full relative cursor-ns-resize shadow-lg transition-colors"
         >
           {/* Knob */}
           <div className="w-[75px] h-[75px] bg-gray-800 border-2 rounded-full">
@@ -89,12 +96,25 @@ export default function Knob({
               className="w-full h-full absolute top-0 left-0 rounded-full pointer-events-none"
               style={{ transform: `rotate(${rotation}deg)` }}
             >
-              <div className="w-1.5 h-3 bg-white mx-auto mt-2 rounded-full group-active:bg-yellow-400 shadow-[0_0_5px_white]" />
+              <div className="w-1.5 h-3 bg-white mx-auto mt-2 rounded-full shadow-[0_0_5px_white]" />
             </div>
           </div>
         </div>
       </div>
       <div className="mt-1 justify-center">{title}</div>
+
+      {/* Spinbox for the knob */}
+      <div className="flex flex-col items-center gap-1 font-semibold">
+        <input
+          type="number"
+          className="w-32 pt-1 pb-1 pl-2 pr-2 text-center bg-white border border-gray-400 rounded text-sm"
+          value={value.toFixed(2)}
+          onChange={(e) => handleSpinboxChange(e)}
+          min={min}
+          max={max}
+          step={step}
+        />
+      </div>
     </div>
   );
 }
