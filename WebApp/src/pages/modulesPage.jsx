2cf87bdaa601b7676b805d472f192a4f91db82fb
Danielle Chang <danielle.c134@gmail.com>
2026-03-10 02:05:25 -0700

    feat: add new modules to channels

--- WebApp/src/pages/modulesPage.jsx
+++ WebApp/src/pages/modulesPage.jsx
@@ -26,27 +26,9 @@ const ModulesPage = () => {
   // UI stuff
   const [selectedChannel, setSelectedChannel] = useState(0);
   const [displayedModules, setDisplayedModules] = useState([]);
+  const [moduleToAdd, setModuleToAdd] = useState(0);
+  const [showAddModuleError, setShowAddModuleError] = useState(false);
 
-  /**
-   * @brief Updates the selected channel for displayed modules
-   * 
-   * @param {*} e - Changed event for the channel dropdown component
-   */
-  const handleDropdownChange = (e) => {
-    setSelectedChannel(e.target.value);
-  };
-
-  /**
-   * @brief Gets the analysis config from the web server
-   */
-  const getSettings = async () => {
-    const res = await api("GET", "/analysis/getSettings");
-
-    if (res.status == HTTP_STATUS.OK) {
-      setGlobalSettings(res.data?.global ?? {});
-      setModules(res.data?.modules ?? []);
-    }
-  };
   /**
    * @brief Updates the indices of the displayed modules according to the selected channel number
    */
@@ -73,6 +55,56 @@ const ModulesPage = () => {
     getSettings();
   }, []);
 
+  /**
+   * @brief Updates the selected channel for displayed modules
+   * 
+   * @param {*} e - Changed event for the channel dropdown component
+   */
+  const handleOutputDropdownChange = (e) => {
+    setSelectedChannel(e.target.value);
+    setShowAddModuleError(false);
+  };
+
+  /**
+   * @brief Updates the selected module to add
+   * 
+   * @param {*} e - Changed event for the add module dropdown component
+   */
+  const handleAddDropdownChange = (e) => {
+    setModuleToAdd(e.target.value);
+    setShowAddModuleError(false);
+  }
+
+  /**
+   * @brief Adds new module of a selected type to the module list
+   */
+  const handleAddModule = () => {
+    // prevent user from adding duplicate modules to the same output
+    if (modules.some(item => item["moduleType"] == moduleToAdd && item["outputNumber"] == selectedChannel)){
+      setShowAddModuleError(true);
+      return;
+    }
+
+    // add module from default registry assigned to current channel
+    const newModule = { ...moduleRegistry[moduleToAdd] };
+    newModule["outputNumber"] = selectedChannel;
+    setModules([...modules, newModule]);
+
+    setShowAddModuleError(false);
+  }
+
+  /**
+   * @brief Gets the analysis config from the web server
+   */
+  const getSettings = async () => {
+    const res = await api("GET", "/analysis/getSettings");
+
+    if (res.status == HTTP_STATUS.OK) {
+      setGlobalSettings(res.data?.global ?? {});
+      setModules(res.data?.modules ?? []);
+    }
+  };
+
   return (
     <div className="flex flex-col m-8">
       <ConfigManager>
@@ -85,12 +117,36 @@ const ModulesPage = () => {
             <h4 className="font-bold text-lg">Output Channel:</h4>
             <select
               className="p-1 h-10 w-20 bg-blue-300 mb-4"
-              onChange={(e) => handleDropdownChange(e)}
+              onChange={(e) => handleOutputDropdownChange(e)}
             >
               <option value={0}>0</option>
               <option value={1}>1</option>
             </select>
           </div>
+          <h4 className="font-bold text-lg">Add New Module:</h4>
+          <div className="flex flex-col mb-4">
+            <div className="flex">
+              <select
+                className="p-1 h-10 w-40 bg-blue-300 mb-2 mr-4"
+                onChange={(e) => handleAddDropdownChange(e)}
+              >
+                <option value={0}>Major Peaks</option>
+                <option value={1}>Percussion</option>
+              </select>
+              <button
+                className="bg-amber-500 cursor-pointer w-30 h-10"
+                onClick={handleAddModule}
+              >
+                Add Module
+              </button>
+            </div>
+            {showAddModuleError && (
+              <p className="text-red-500 text-sm">
+                This module has already been added to channel {selectedChannel}
+              </p>
+            )}
+          </div>
+
           <div className="flex flex-row gap-4">
             {/* Only display modules with the corresponding output channel number.
                 We pass in modules[index] because the actual modules being sent to the server
--- WebApp/src/utils/defaultModules.js
+++ WebApp/src/utils/defaultModules.js
@@ -2,7 +2,7 @@ export const moduleRegistry = {
   0: { // major peaks
     "moduleType": 0,
     "freqLow": 400,
-    "freqhigh": 1000,
+    "freqHigh": 1000,
     "minAmpNorm": 10000.0,
     "maxPeaks": 1,
     "frequencyMapping": 1
@@ -10,7 +10,7 @@ export const moduleRegistry = {
   1: { // percussion
     "moduleType": 1,
     "freqLow": 1800,
-    "freqhigh": 4000,
+    "freqHigh": 4000,
     "minAmpNorm": 10000000.0,
     "fluxThresh": 0.5,
     "energyThresh": 100000000.0,
